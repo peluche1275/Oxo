5aa5235e5afb225a0fc595c59315846eda4b7899
Nathan <nathan.altomare.belier@gmail.com>
2020-07-10 10:45:10 +0200

    Adding

--- Web/JS/TicTacToe.js
+++ Web/JS/TicTacToe.js
@@ -3,6 +3,16 @@ class TicTacToe {
     constructor() {
 
         this.gridBoxes = document.getElementsByClassName("box");
+        this.buttonRestart = document.getElementById("restart");
+        this.location = document.location;
+
+    }
+
+    run() {
+
+        this.touchGridBoxes();
+
+        this.restartGame();
 
     }
 
@@ -13,15 +23,25 @@ class TicTacToe {
         for (let i = 0; i < 9; i++) {
 
             let boxSelected = self.gridBoxes.item(i);
-            
+
             boxSelected.addEventListener("click", function () {
                 boxSelected.innerHTML = "X";
             });
 
         }
     }
+
+    restartGame() {
+
+        let self = this;
+
+        this.buttonRestart.addEventListener("click", function () {
+            self.location.reload(true);
+        });
+
+    }
 }
 
 let Oxo = new TicTacToe;
-Oxo.touchGridBoxes();
+Oxo.run();
 
